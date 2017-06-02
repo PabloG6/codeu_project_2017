bfcf9eed15c692fdebd3dcc51368657f6b297e91
root <root@Lenovo-PC.localdomain>
2017-06-01 22:38:53 -0400

    Committing addition of getServerInfo method from BasicView.jAVA interface to View.java

--- src/codeu/chat/server/View.java
+++ src/codeu/chat/server/View.java
@@ -27,12 +27,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import codeu.chat.common.BasicView;
-import codeu.chat.common.ConversationHeader;
-import codeu.chat.common.ConversationPayload;
-import codeu.chat.common.Message;
-import codeu.chat.common.SinglesView;
-import codeu.chat.common.User;
+import codeu.chat.common.*;
 import codeu.chat.util.Logger;
 import codeu.chat.util.Time;
 import codeu.chat.util.Uuid;
@@ -48,6 +43,11 @@ public final class View implements BasicView, SinglesView {
     this.model = model;
   }
 
+
+  @Override
+  public ServerInfo getServerInfo() {
+    return null;
+  }
 
   @Override
   public Collection<User> getUsers() {
