570b8b8f713c8ed373354f93b1c2e4fb353eadc5
charlinds <cal285@georgetown.edu>
2017-06-24 20:56:53 -0400

    Update Model.java
    support follow methods

--- src/codeu/chat/server/Model.java
+++ src/codeu/chat/server/Model.java
@@ -1,3 +1,22 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Marketplace
+Gist
+ @charlinds
+ Sign out
+ Watch 3
+  Star 0
+  Fork 0 jedouard98/codeu_summer_2017
+ Code  Issues 0  Pull requests 2  Projects 0  Wiki Insights 
+Tree: 6ec1573535 Find file Copy pathcodeu_summer_2017/src/codeu/chat/server/Model.java
+6ec1573  9 hours ago
+@jedouard98 jedouard98 Updated model to support follow methods
+2 contributors @jedouard98 @vaage
+RawBlameHistory      
+184 lines (145 sloc)  6.12 KB
 // Copyright 2017 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +34,7 @@
 package codeu.chat.server;
 
 import java.util.Comparator;
+import java.util.HashMap;
 
 import codeu.chat.common.ConversationHeader;
 import codeu.chat.common.ConversationPayload;
@@ -67,10 +87,63 @@ public final class Model {
   private final Store<Time, Message> messageByTime = new Store<>(TIME_COMPARE);
   private final Store<String, Message> messageByText = new Store<>(STRING_COMPARE);
 
+  private final HashMap<Uuid, HashMap<Uuid, Integer>> userConversationTracking = new HashMap<Uuid, HashMap<Uuid, Integer>>();
+
+  private final String version = "1.1";
+  private final Time serverStartTime = Time.now();
+
   public void add(User user) {
+    userConversationTracking.put(user.id, new HashMap<Uuid, Integer>());
     userById.insert(user.id, user);
     userByTime.insert(user.creation, user);
     userByText.insert(user.name, user);
+  }
+
+  public String statusUpdate(Uuid user) {
+    StringBuilder status = new StringBuilder();
+    HashMap<Uuid, Integer> userConversationSize = userConversationTracking.get(user);
+    for (Uuid conversation : userConversationSize.keySet()) {
+      ConversationHeader convo = conversationById().first(conversation);
+      String title = convo.title;
+      int newMessages = convo.size - userConversationSize.get(conversation);
+      String line = String.format("CONVERSATION %s: You have %d new messages!\n", title, newMessages);
+      status.append(line);
+      userConversationTracking.get(user).put(conversation, convo.size);
+    }
+    User userA = userById().first(user);
+    status.append(userA.statusUpdate());
+    return status.toString();
+  }
+
+  public void unfollowUser(User userA, User userB) {
+    User user1 = userById().first(userA.id);
+    User user2 = userById().first(userB.id);
+    User.unfollow(user1, user2);
+  }
+
+  public void followUser(User userA, User userB) {
+    User user1 = userById().first(userA.id);
+    User user2 = userById().first(userB.id);
+    User.follow(user1, user2);
+  }
+
+  public void unfollowConversation(Uuid user, Uuid conversation) {
+    userConversationTracking.get(user).remove(conversation);
+  }
+
+  public void followConversation(Uuid user, Uuid conversation) {
+    // Put into hashmap the conversation and what the size of the conversation
+    // is for the user at the time of following
+    ConversationHeader convo = conversationById().first(conversation);
+    userConversationTracking.get(user).put(conversation, convo.size);
+  }
+
+  public long uptime() {
+    return Time.now().inMs() - serverStartTime.inMs();
+  }
+
+  public String version() {
+    return version;
   }
 
   public StoreAccessor<Uuid, User> userById() {
@@ -85,7 +158,8 @@ public final class Model {
     return userByText;
   }
 
-  public void add(ConversationHeader conversation) {
+  public void add(User user, ConversationHeader conversation) {
+    user.addCreatedConversation(conversation);
     conversationById.insert(conversation.id, conversation);
     conversationByTime.insert(conversation.creation, conversation);
     conversationByText.insert(conversation.title, conversation);
@@ -126,3 +200,5 @@ public final class Model {
     return messageByText;
   }
 }
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
